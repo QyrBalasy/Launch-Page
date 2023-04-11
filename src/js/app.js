ddb593850b0cdee3c61179a963d7861a9363d512
qyrBalasy <104926205+QyrBalasy@users.noreply.github.com>
2023-04-11 19:12:25 +0600

    chore: add form & small fix

--- src/js/app.js
+++ src/js/app.js
@@ -2,22 +2,18 @@ import * as flsFunctions from "./modules/functions.js";
 import * as phoneInput from "./modules/phoneinput.js";
 // import * as mymodal from "./modules/modal.js";
 import * as burger from "./modules/burger.js";
-// import { formValidate, formValidateBanner } from "./modules/form.js";
+import { formValidate } from "./modules/form.js";
 // import { onEntry } from "./modules/fadeBlocks.js";
 import { MySwiper } from "./modules/swiper.js";
 import { showContent } from "./modules/accordion.js";
 import runSplineAnimation from "./modules/scene.js";
 
-
 flsFunctions.isWebp();
 burger.burgerMenuCustom();
 runSplineAnimation();
+showContent();
 phoneInput.isPhone();
 // mymodal.modal();
 // onEntry();
-showContent();
 MySwiper();
-// formValidate();
-// formValidateBanner();
-
-
+formValidate();
