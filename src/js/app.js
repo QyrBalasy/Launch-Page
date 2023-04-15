2adc9cd1ec43e6cb8b32b01bf94ee899b5445227
qyrBalasy <104926205+QyrBalasy@users.noreply.github.com>
2023-04-15 22:01:41 +0600

    chore: minor fixes

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 import * as flsFunctions from "./modules/functions.js";
 import * as phoneInput from "./modules/phoneinput.js";
-// import * as mymodal from "./modules/modal.js";
 import * as burger from "./modules/burger.js";
+import { initModal, formModal } from "./modules/modal.js";
 import { formValidate } from "./modules/form.js";
 // import { onEntry } from "./modules/fadeBlocks.js";
 import { MySwiper } from "./modules/swiper.js";
@@ -13,7 +13,19 @@ burger.burgerMenuCustom();
 runSplineAnimation();
 showContent();
 phoneInput.isPhone();
-// mymodal.modal();
+formModal();
+initModal({
+  modal: ".modal",
+  modalImg: ".modal .portfolio-card__big",
+  modalTitle: ".modal .modal-text__title",
+  modalDescription: ".modal .modal-text__description",
+});
 // onEntry();
 MySwiper();
-formValidate();
+const form1 = document.forms.form1;
+formValidate(form1);
+
+
+
+
+
--- src/js/modules/modal.js
+++ src/js/modules/modal.js
@@ -1,10 +1,11 @@
-export function modal() {
-  let modal = document.querySelector(".modal");
-  let trigger = document.querySelectorAll(".trigger");
-  let closeButton = document.querySelector(".close-button");
+export function formModal() {
+  let modal = document.querySelector(".contactForm");
+  let triggers = document.querySelectorAll(".form-trigger");
+  let closeButton = document.querySelector(".contactForm__dots");
 
   function toggleModal() {
     modal.classList.toggle("show-modal");
+    document.body.classList.toggle("no-scroll");
   }
 
   function windowOnClick(event) {
@@ -13,10 +14,75 @@ export function modal() {
     }
   }
 
-  for (let i = 0; i < trigger.length; i++) {
-    trigger[i].addEventListener("click", toggleModal);
-  }
+  // for (let i = 0; i < trigger.length; i++) {
+  //   trigger[i].addEventListener("click", toggleModal);
+  // }
+
+  triggers.forEach(function (trigger) {
+    trigger.addEventListener("click", toggleModal);
+  });
 
   closeButton.addEventListener("click", toggleModal);
   window.addEventListener("click", windowOnClick);
+}
+
+export function initModal(modalSelectors) {
+  const modal = document.querySelector(modalSelectors.modal);
+  if (!modal) {
+    console.error("Modal element is not found!");
+    return;
+  }
+  const modalImg = modalSelectors.modalImg
+    ? modal.querySelector(modalSelectors.modalImg)
+    : null;
+  const modalTitle = modalSelectors.modalTitle
+    ? modal.querySelector(modalSelectors.modalTitle)
+    : null;
+  const modalDescription = modalSelectors.modalDescription
+    ? modal.querySelector(modalSelectors.modalDescription)
+    : null;
+
+  const body = document.body;
+
+  function openModal(modalId, titleImg, descriptionImg) {
+    const slideImg = document.querySelector(`[data-modal="${modalId}"] img`);
+
+    modalImg.src = slideImg.src;
+    modalTitle.textContent = titleImg;
+    modalDescription.textContent = descriptionImg;
+    modal.classList.add("show-modal");
+    // document.body.classList.toggle("no-scroll");
+    body.style.overflow = "hidden";
+  }
+
+  function closeModal() {
+    modal.classList.remove("show-modal");
+    // modalImg.src = "";
+    // modalTitle.textContent = "";
+    // modalDescription.textContent = "";
+    body.style.overflow = "auto";
+  }
+  function modalSwiper() {
+    const slideTriggers = document.querySelectorAll(".trigger");
+
+    slideTriggers.forEach((slideTrigger) => {
+      slideTrigger.addEventListener("click", () => {
+        const modalId = slideTrigger.getAttribute("data-modal");
+        const titleImg = slideTrigger.getAttribute("data-title");
+        const descriptionImg = slideTrigger.getAttribute("data-description");
+        openModal(modalId, titleImg, descriptionImg);
+      });
+    });
+
+    const closeButton = modal.querySelector(".close-button");
+    closeButton.addEventListener("click", closeModal);
+
+    window.addEventListener("click", (event) => {
+      if (event.target === modal) {
+        closeModal();
+      }
+    });
+  }
+
+  modalSwiper();
 }
